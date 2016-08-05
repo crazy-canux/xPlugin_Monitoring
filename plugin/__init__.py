01d4f11ed8cad0e66e1f3025b9dc22d67b429dc8
Canux CHENG <canux.cheng@faurecia.com>
2016-08-05 15:34:19 +0800

    [update]register to pypi

---
+++ plugin/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Copyright (C) 2015 Faurecia (China) Holding Co.,Ltd.
+
+All rights reserved.
+Name: __init__.py
+Author: Canux CHENG canuxcheng@gmail.com
+Version: V1.0.0.0
+Time: Fri 05 Aug 2016 02:41:19 PM CST
+
+Description:
+"""
+
+__VERSION__ = "1.0.0.0"
--- setup.py
+++ setup.py
@@ -1,15 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-######################################################################
-# Copyright (C) 2015 Faurecia (China) Holding Co.,Ltd.               #
-# All rights reserved                                                #
-# Name: setup.py
-# Author: Canux canuxcheng@gmail.com                                 #
-# Version: V1.0                                                      #
-# Time: Tue 23 Feb 2016 02:39:34 AM EST
-######################################################################
-# Description:
-######################################################################
+"""Copyright (C) 2015 Faurecia (China) Holding Co.,Ltd.
 
-from setuptools import setup
-from setuptools import find_packages
+All rights reserved.
+Name: setup.py
+Author: Canux CHENG canuxcheng@gmail.com
+Version: V1.0.0.0
+Time: Fri 05 Aug 2016 09:59:29 AM CST
+
+Description:
+"""
+import os
+
+from setuptools import setup, find_packages
+
+import plugin
+
+INSTALL_REQUIRES = [
+    'pymysql',
+    'pymssql',
+    'pysnmp',
+    'paramiko'
+]
+
+setup(
+    name='xplugin',
+    version=plugin.__VERSION__,
+    author='Canux CHENG',
+    author_email='canuxcheng@gmail.com',
+    maintainer='Canux CHENG',
+    maintainer_email='canuxcheng@gmail.com',
+    description='Common interface for tons of protocal, used for monitoring tools, like nagios/icinga...',
+    long_description=open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'README'))).read(),
+    license='GPL',
+    platforms='any',
+    keywords='monitoring nagios plugin',
+    url='https://github.com/crazy-canux/xPlugin_Monitoring',
+    install_requires=INSTALL_REQUIRES,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: System :: Monitoring"
+    ],
+)
