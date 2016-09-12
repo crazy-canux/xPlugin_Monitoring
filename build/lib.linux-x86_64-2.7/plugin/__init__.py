3ce7aff4253f8f57c2af5919d93db085674df656
Canux CHENG <canuxcheng@gmail.com>
2016-09-12 03:43:55 -0400

    release

--- build/lib.linux-x86_64-2.7/plugin/__init__.py
+++ build/lib.linux-x86_64-2.7/plugin/__init__.py
@@ -9,10 +9,7 @@ Name: __init__.py
 Author: Canux CHENG canuxcheng@gmail.com
 Version: V1.0.0.0
 Time: Fri 05 Aug 2016 02:41:19 PM CST
-
-Example:
-    import plugin
 """
 
-__version__ = "2.0.0.0"
+__version__ = "2.0.1.0"
 __author__ = "Canux CHENG"
---
+++ plugin/plugins/exchange_2010/README.rst
@@ -0,0 +1,15 @@
+Exchange 2010 plugins
+=====================
+
+These are a collection of plugins used by Nagios to monitor Exhange
+2010.
+
+Run tests
+---------
+
+Run the test suite from project root with:
+
+::
+
+    $ nosetests
+
